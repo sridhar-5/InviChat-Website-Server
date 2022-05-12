fed41f5b105db2a90c8b0f6b8d98f01c3acfe08b
Venkata Sridhar Sai Kalavalapalli <kv.sridharsai@gmail.com>
2022-05-12 12:15:43 +0530

    final commit amazon sambav website server

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config();
+const products = require("./Models/productDetails");
+
+function connectDatabase() {
+  const DatabaseConnection = mongoose.connect(
+    `mongodb+srv://${process.env.USERNAME}:${process.env.PASSWORD}@sihhack.0ea71.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
+  );
+
+  DatabaseConnection.then(() => {
+    console.log("Connection to the database successfully");
+  });
+  DatabaseConnection.catch((error) => {
+    console.log(`Connection Refused...${error}`);
+  });
+}
+
+connectDatabase();
+
+app.use(express.json());
+
+app.get("/", async (request, response) => {
+  const getAllTheProducts = await products.find({});
+  if (process.env.NODE_ENV !== "production") {
+    console.log(getAllTheProducts);
+  }
+
+  response.status(200).send(getAllTheProducts);
+});
+
+const PORT = process.env.PORT || 8000;
+
+app.listen(PORT, () => {
+  console.log(`server started on ${PORT}`);
+});
