8271285c3257d88e65f67733a5de0a851c8cc8c5
Venkata Sridhar Sai Kalavalapalli <kv.sridharsai@gmail.com>
2022-05-22 19:33:25 +0530

    final commit

--- index.js
+++ index.js
@@ -6,7 +6,13 @@ dotenv.config();
 const products = require("./Models/productDetails");
 var cors = require("cors");
 
-app.use(cors());
+const corsOptions = {
+  origin: "http://localhost:3000",
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
 
 function connectDatabase() {
   const DatabaseConnection = mongoose.connect(
