d633ecf72a3e4e4a8a9d57a8868e7a8bec82059e
Venkata Sridhar Sai Kalavalapalli <kv.sridharsai@gmail.com>
2022-05-23 18:56:35 +0530

    fixed cors issue

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const products = require("./Models/productDetails");
 var cors = require("cors");
 
 const corsOptions = {
-  origin: "http://localhost:3000",
+  origin: "*",
   credentials: true, //access-control-allow-credentials:true
   optionSuccessStatus: 200,
 };
