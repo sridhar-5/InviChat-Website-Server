229c87c3d7ab02f70cb17fe60e372afb9c32b420
Venkata Sridhar Sai Kalavalapalli <kv.sridharsai@gmail.com>
2022-06-09 23:56:25 +0530

    few changes

--- routes/checkSellerExists.js
+++ routes/checkSellerExists.js
@@ -2,9 +2,11 @@ const express = require("express");
 const mongoose = require("mongoose");
 const router = express.Router();
 const Sellers = require("../Models/Seller.js");
+const url = require("url");
 
-router.get("/:phone", async (request, response) => {
-  const { phone } = request.params;
+router.get("/", async (request, response) => {
+  const queryObject = url.parse(request.url, true).query;
+  const { phone } = queryObject;
   const findSeller = await Sellers.find({ phone });
   if (findSeller) {
     response.status(200).send(findSeller);
