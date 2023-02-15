f34abcd4d4bbe2386fcc0536cdb2eba321dec2f0
Venkata Sridhar Sai Kalavalapalli <kv.sridharsai@gmail.com>
2023-02-15 18:12:53 +0530

    changed endpoint getProduct to product

--- index.js
+++ index.js
@@ -9,13 +9,8 @@ var cors = require("cors");
 const postSellers = require("./routes/createSellers.js");
 const findSeller = require("./routes/checkSellerExists.js");
 const findProd = require("./routes/Products.js");
-const corsOptions = {
-  origin: "*",
-  credentials: true, //access-control-allow-credentials:true
-  optionSuccessStatus: 200,
-};
 
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(express.json());
 
 function connectDatabase() {
